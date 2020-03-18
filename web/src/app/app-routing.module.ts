0392979c8f621314c0ac0371e060de7d24a29154
viidtkhemka63 <viditkhemka63@gmail.com>
2020-03-18 17:31:38 +0530

    login page UI

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { SignupComponent } from './components/signup/signup.component';
 
 
 const routes: Routes = [
+  {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'dashboard', component: DashboardComponent},
